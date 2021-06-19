17b344a74220cca20c9e4fb9d955aecfa7bb2021
Rubin <rjhand@gmail.com>
2021-06-19 17:43:57 +0000

    Server: Create config.js and pass env variables for database setup.  If using ES6 is not possible, revert to commonJS

---
+++ server/src/db/config/config.js
@@ -0,0 +1,29 @@
+import '../../utils/env.js';
+
+// ES6 not used because db:create would not work
+// config.json worked, but exposed env variables
+
+module.exports = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    host: process.env.DB_HOST,
+    dialect: 'postgres'
+  },
+  test: {
+    username: 'root',
+    password: null,
+    database: 'database_test',
+    host: '127.0.0.1',
+    dialect: 'postgres'
+  },
+  production: {
+    username: 'root',
+    password: null,
+    database: 'database_production',
+    host: '127.0.0.1',
+    dialect: 'postgres'
+  }
+};
