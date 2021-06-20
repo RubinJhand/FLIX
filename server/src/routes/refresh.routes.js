cb9e79fe7433b115acf6f1393efa74dbbd380826
Rubin <rjhand@gmail.com>
2021-06-20 20:08:04 +0000

    Server: Update routes to refect changes in media controller

--- server/src/routes/refresh.routes.js
+++ server/src/routes/refresh.routes.js
@@ -1,12 +1,9 @@
 import { Router } from 'express';
-import {
-  mediaDeleteAll,
-  mediaCreateBulk
-} from '../controllers/refreshDB.controller';
+import { mediaDeleteAll, mediaCreateBulk } from '../controllers';
 
 const router = Router();
 
 router.get('/delete', mediaDeleteAll);
-router.get('/createBulk', mediaCreateBulk);
+router.get('/create', mediaCreateBulk);
 
 export default router;
