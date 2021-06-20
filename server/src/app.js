f3b5bda56c768a661e935e8835d5fc4f626b5f37
Rubin <rjhand@gmail.com>
2021-06-20 07:16:20 +0000

    Server: Update app.js file to include the use of constants for the base API and to include routes for the refreshDB controller.

--- server/src/app.js
+++ server/src/app.js
@@ -2,7 +2,13 @@ import express, { json, urlencoded } from 'express';
 import cors from 'cors';
 
 import { CORS_ORIGIN } from './constants/cors.js';
+import {
+  API_CURRENT_VERSION,
+  DB_REFRESH_PATH
+} from './constants/apiVersion.js';
+
 import MediaRoutes from '../src/routes/media.routes.js';
+import MediaRefresh from '../src/routes/refresh.routes.js';
 
 const app = express();
 
@@ -12,7 +18,8 @@ app.use(urlencoded({ extended: true }));
 app.use(cors(CORS_ORIGIN));
 
 // ----- Routes -----
-app.use('/api/v1', MediaRoutes);
+app.use(API_CURRENT_VERSION, MediaRoutes);
+app.use(`${DB_REFRESH_PATH}`, MediaRefresh);
 
 app.get('/', (req, res) => {
   res.json({ data: 'working!' });
