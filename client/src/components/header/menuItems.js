d4093257ec14817ac65ad35c1ed7f2fcbb045ebf
Rubin J <rjhand@gmail.com>
2021-06-26 09:59:56 -0700

    Client: Create a file for menu items lists for the header and navbar components.

---
+++ client/src/components/header/menuItems.js
@@ -0,0 +1,13 @@
+export const mainMenuItems = [
+  { title: 'Trending' },
+  { title: 'Movies' },
+  { title: 'TV Shows' }
+];
+
+export const subMenuItems = [
+  { title: 'Amazon Prime' },
+  { title: 'Netflix' },
+  { title: 'hulu' },
+  { title: 'HBO Max' },
+  { title: 'Disney+' }
+];
