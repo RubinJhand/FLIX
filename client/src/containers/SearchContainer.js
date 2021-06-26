47ed45b5451c6030c5d65f55760f429e2b9897bf
Rubin J <rjhand@gmail.com>
2021-06-26 15:50:21 -0700

    Client: Create a function which will determine the opening and closing of the search bar.  If the the search is open, change icon.

--- client/src/containers/SearchContainer.js
+++ client/src/containers/SearchContainer.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import SearchBar from '../components/searchBar/SearchBar';
 
+import SearchIcon from '@material-ui/icons/Search';
+import CloseIcon from '@material-ui/icons/Close';
+
 const SearchContainer = ({ isMobileMenu }) => {
+  const [isSearchOpen, setIsSearchOpen] = useState(false);
+
+  const handleOpenSearch = (e) => {
+    setIsSearchOpen((isSearchOpen) => !isSearchOpen);
+  };
+
   return (
-    <SearchBar>
-      <SearchBar.Form>
+    <SearchBar isSearchOpen={isSearchOpen}>
+      <SearchBar.Form isSearchOpen={isSearchOpen}>
         <SearchBar.Input />
       </SearchBar.Form>
-      <SearchBar.Span></SearchBar.Span>
+      <SearchBar.Span onClick={handleOpenSearch}>
+        {isSearchOpen ? <CloseIcon /> : <SearchIcon />}
+      </SearchBar.Span>
     </SearchBar>
   );
 };
