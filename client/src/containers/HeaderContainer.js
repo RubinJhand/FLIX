68b0ae5fe0a35833ac7e3f187ffa45f5893ba7ca
Rubin J <rjhand@gmail.com>
2021-06-26 08:32:57 -0700

    Client: Add unstyled content to Header.

--- client/src/containers/HeaderContainer.js
+++ client/src/containers/HeaderContainer.js
@@ -2,12 +2,25 @@ import React from 'react';
 
 import Header from '../components/header/Header';
 
+import { mainMenuItems } from '../components/header/menuItems';
+
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
 const HeaderContainer = () => {
   return (
     <Header>
-      <Header.Left></Header.Left>
+      <Header.Left>
+        <Header.Logo />
 
-      <Header.Middle></Header.Middle>
+        <Header.Text>
+          Browse
+          <ExpandMoreIcon />
+        </Header.Text>
+      </Header.Left>
+
+      <Header.Middle>
+        <Header.Menu list={mainMenuItems} />
+      </Header.Middle>
 
       <Header.Right></Header.Right>
     </Header>
