f00f877559693697336becf0f6038a41287c7a55
Rubin J <rjhand@gmail.com>
2021-06-27 14:20:52 -0700

    Client: Import the Navbar container. Add functionality for changing the media type using the useMediaType hook. Set the final iteration of the app's logo on the left side of the header.

--- client/src/containers/HeaderContainer.js
+++ client/src/containers/HeaderContainer.js
@@ -1,16 +1,24 @@
 import React, { useState, useEffect } from 'react';
+
+// Hooks and Contexts
+import useMediaType from '../hooks/useMediaType';
 import useMobileMenu from '../hooks/useMobileMenu';
 
+// Components
 import Header from '../components/header/Header';
 import SearchContainer from './SearchContainer';
+import NavbarContainer from './NavbarContainer';
 
-import { mainMenuItems } from '../components/header/menuItems';
-
+// Styles
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
+// Misc.
+import { mainMenuItems } from '../components/header/header.menuItems';
 
 const HeaderContainer = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [isMobileMenu] = useMobileMenu('(max-width: 700px)');
+  const { setMediaType } = useMediaType();
 
   useEffect(() => {
     if (!isMobileMenu) {
@@ -25,10 +33,19 @@ const HeaderContainer = () => {
     setIsMenuOpen((isMenuOpen) => !isMenuOpen);
   };
 
+  const handleChangeMediaType = (type) => {
+    type && setMediaType(type);
+    type && isMobileMenu && setIsMenuOpen(false);
+  };
+
   return (
     <Header>
       <Header.Left>
-        <Header.Logo />
+        <Header.LogoWrapper>
+          <Header.LogoText>fl</Header.LogoText>
+          <Header.Logo />
+          <Header.LogoText>x</Header.LogoText>
+        </Header.LogoWrapper>
         {isMobileMenu && (
           <Header.Text onClick={handleOpenMenu} isMenuOpen={isMenuOpen}>
             Browse
@@ -38,16 +55,23 @@ const HeaderContainer = () => {
       </Header.Left>
 
       <Header.Middle>
-        <Header.Menu
-          isMobileMenu={isMobileMenu}
-          isMenuOpen={isMenuOpen}
-          list={mainMenuItems}
-        />
+        <Header.Menu isMobileMenu={isMobileMenu} isMenuOpen={isMenuOpen}>
+          {mainMenuItems.map((item, index) => (
+            <Header.ListItem
+              key={index + item.title}
+              isMobileMenu={isMobileMenu}
+              onClick={() => handleChangeMediaType(item.type)}
+            >
+              {item.title}
+            </Header.ListItem>
+          ))}
+        </Header.Menu>
       </Header.Middle>
 
       <Header.Right>
         <SearchContainer isMobileMenu={isMobileMenu} />
       </Header.Right>
+      <NavbarContainer isMobileMenu={isMobileMenu} />
     </Header>
   );
 };
