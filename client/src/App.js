26224d1421cf732d68adc7719efce0d52bddb207
Rubin J <rjhand@gmail.com>
2021-06-27 16:19:31 -0700

    Client: Add routing and providers (context).

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,32 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 
+import { ApiDataProvider } from './contexts/apiDataContext';
+import { MediaTypeProvider } from './contexts/mediaTypeContext';
 import HeaderContainer from './containers/HeaderContainer';
 
 const App = () => {
   return (
-    <div>
-      {/* Header */}
-      <HeaderContainer />
-      {/* Navbar */}
-      {/* Hero/Carousel */}
-      {/* Main */}
-    </div>
+    <>
+      <ApiDataProvider>
+        <MediaTypeProvider>
+          <HeaderContainer />
+        </MediaTypeProvider>
+        <Switch>
+          <Route exact path='/'>
+            {/* Main */}
+          </Route>
+          <Route exact path='/tv/:genre'>
+            {/* Carousel */}
+            {/* Main */}
+          </Route>
+          <Route exact path='/movie/:genre'>
+            {/* Carousel */}
+            {/* Main */}
+          </Route>
+        </Switch>
+      </ApiDataProvider>
+    </>
   );
 };
 
