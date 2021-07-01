057dcd61abc60d95daab7dd20ff2e230ead2d487
Rubin J <rjhand@gmail.com>
2021-07-01 10:03:33 -0700

    Client: Determine what the card slayout will be and create components for use in the card container.

--- client/src/components/card/Card.js
+++ client/src/components/card/Card.js
@@ -1,11 +1,74 @@
 import React from 'react';
-import { Box, Border } from './card.styles';
+import {
+  Box,
+  Border,
+  Image,
+  Trailer,
+  TrailerWrapper,
+  Title,
+  Overview,
+  Text,
+  LogoWrapper,
+  InfoWrapper,
+  LogoImage,
+  Section,
+  Categories,
+  Seperator
+} from './card.styles';
 
 const Card = ({ children, ...restProps }) => {
   return <Box {...restProps}>{children}</Box>;
 };
 Card.Border = ({ children, ...restProps }) => {
   return <Border {...restProps}>{children}</Border>;
+};
+
+Card.Image = ({ src, alt, ...restProps }) => {
+  return <Image src={src} alt={alt} {...restProps} />;
+};
+
+Card.Trailer = ({ src, alt, ...restProps }) => {
+  return <Trailer src={src} alt={alt} {...restProps} />;
+};
+
+Card.TrailerWrapper = ({ children, ...restProps }) => {
+  return <TrailerWrapper {...restProps}>{children}</TrailerWrapper>;
+};
+
+Card.LogoImage = ({ src, alt, ...restProps }) => {
+  return <LogoImage src={src} alt={alt} {...restProps} />;
+};
+
+Card.Overview = ({ children, ...restProps }) => {
+  return <Overview {...restProps}>{children}</Overview>;
+};
+
+Card.Categories = ({ children, ...restProps }) => {
+  return <Categories {...restProps}>{children}</Categories>;
+};
+
+Card.Title = ({ children, ...restProps }) => {
+  return <Title {...restProps}>{children}</Title>;
+};
+
+Card.Text = ({ children, ...restProps }) => {
+  return <Text {...restProps}>{children}</Text>;
+};
+
+Card.InfoWrapper = ({ children, ...restProps }) => {
+  return <InfoWrapper {...restProps}>{children}</InfoWrapper>;
+};
+
+Card.LogoWrapper = ({ children, ...restProps }) => {
+  return <LogoWrapper {...restProps}>{children}</LogoWrapper>;
+};
+
+Card.Section = ({ children, ...restProps }) => {
+  return <Section {...restProps}>{children}</Section>;
+};
+
+Card.Seperator = ({ children, ...restProps }) => {
+  return <Seperator {...restProps}>{children}</Seperator>;
 };
 
 export default Card;
