492012f85ddb6cab515e07bc837bcd32cb1c4393
Rubin J <rjhand@gmail.com>
2021-07-01 15:53:35 -0700

    Client: Create a slides component which will display an image carousel with a title and a brief overview.

---
+++ client/src/components/slides/Slides.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import {
+  Container,
+  Inner,
+  ImageWrapper,
+  Image,
+  Button,
+  TextWrapper
+} from './slides.styles';
+
+const Slides = ({ children, ...restProps }) => {
+  return <Container {...restProps}>{children}</Container>;
+};
+
+Slides.Inner = ({ children, ...restProps }) => {
+  return <Inner {...restProps}>{children}</Inner>;
+};
+
+Slides.ImageWrapper = ({ children, ...restProps }) => {
+  return <ImageWrapper {...restProps}>{children}</ImageWrapper>;
+};
+
+Slides.TextWrapper = ({ children, ...restProps }) => {
+  return <TextWrapper {...restProps}>{children}</TextWrapper>;
+};
+
+Slides.Image = ({ src, alt, ...restProps }) => {
+  return <Image src={src} alt={alt} {...restProps} />;
+};
+
+Slides.Button = ({ children, ...restProps }) => {
+  return <Button {...restProps}>{children}</Button>;
+};
+
+export default Slides;
