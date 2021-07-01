4e4d096c5cffbc3d7f905d1bb1a4a8f78d6fa0d4
Rubin J <rjhand@gmail.com>
2021-07-01 07:12:40 -0700

    Client: Create an Axios instance .

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,6 +1,7 @@
 import Axios from 'axios';
 
-const DEVELOPMENT = 'http://localhost:3000/api/v1/category';
+// const DEVELOPMENT = 'http://localhost:3000/api/v1/category';
+const DEVELOPMENT = 'http://127.0.0.1:3000/api/v1/category';
 
 export const axios = Axios.create({
   baseURL: DEVELOPMENT
