29955035a660e7999813ed68eac5191a49354662
Rubin J <rjhand@gmail.com>
2021-07-01 17:17:05 -0700

    Client: Create a slides container and make it functional, but not styled.

---
+++ client/src/containers/SlidesContainer.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+// Hooks and Contexts
+import useMediaTimer from '../hooks/useTimedSlides';
+
+// Components
+import Slides from '../components/slides/Slides';
+import Card from '../components/card/Card';
+
+// Styles
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
+import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
+
+const SlidesContainer = () => {
+  const { mediaArray, handleNext, handlePrevious, imgIndex } = useMediaTimer();
+  const overview = mediaArray[imgIndex]?.overview.split('. ');
+
+  return (
+    <Slides>
+      <Slides.Inner>
+        <Slides.TextWrapper>
+          <Card.Title>{mediaArray[imgIndex]?.title}</Card.Title>
+          <Card.Text>{overview && overview[0]}</Card.Text>
+        </Slides.TextWrapper>
+      </Slides.Inner>
+      <Slides.ImageWrapper>
+        <Slides.Image src={mediaArray[imgIndex]?.url} alt={'Not available'} />
+      </Slides.ImageWrapper>
+
+      <Slides.Button onClick={handlePrevious} isBack>
+        <ArrowBackIosIcon />
+      </Slides.Button>
+
+      <Slides.Button onClick={handleNext} isForward>
+        <ArrowForwardIosIcon />
+      </Slides.Button>
+    </Slides>
+  );
+};
+
+export default SlidesContainer;
