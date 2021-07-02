9801eeabde27032ce57fec706d4820d6f26fe4d8
Rubin J <rjhand@gmail.com>
2021-07-02 00:55:25 -0700

    Client: Complete the search container .  Complete search functionality.

--- client/src/containers/SearchContainer.js
+++ client/src/containers/SearchContainer.js
@@ -1,4 +1,9 @@
 import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+
+// Hooks and Contexts
+import useApiData from '../hooks/useApiData';
+import useInput from '../hooks/useInput';
 
 // Components
 import SearchBar from '../components/searchBar/SearchBar';
@@ -8,19 +13,36 @@ import SearchIcon from '@material-ui/icons/Search';
 import CloseIcon from '@material-ui/icons/Close';
 
 const SearchContainer = ({ isMobileMenu }) => {
+  const [, setUrl] = useApiData();
+  const [searchValue, bindSearch, resetSearch] = useInput('');
   const [isSearchOpen, setIsSearchOpen] = useState(false);
+  const history = useHistory();
 
   const handleOpenSearch = (e) => {
     setIsSearchOpen((isSearchOpen) => !isSearchOpen);
   };
 
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
+
+    setUrl(`search/${searchValue}`);
+    setIsSearchOpen(false);
+    resetSearch();
+
+    history.push({
+      pathname: `/search/${searchValue}`
+    });
+  };
+
   return (
     <SearchBar isSearchOpen={isSearchOpen}>
-      <SearchBar.Form isSearchOpen={isSearchOpen}>
+      <SearchBar.Form onSubmit={handleSearchSubmit} isSearchOpen={isSearchOpen}>
         <SearchBar.Input
+          type='text'
+          {...bindSearch}
+          placeholder={'Search...'}
           isSearchOpen={isSearchOpen}
           isMobileMenu={isMobileMenu}
-          placeholder={'Search...'}
         />
         <SearchBar.Span onClick={handleOpenSearch} isSearchOpen={isSearchOpen}>
           {isSearchOpen ? <CloseIcon /> : <SearchIcon />}
