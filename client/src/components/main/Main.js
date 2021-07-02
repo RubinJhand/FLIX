e48a2be6569e92f0e067b80bc124cb12b4dd2162
Rubin J <rjhand@gmail.com>
2021-07-01 19:35:37 -0700

    Client: Create a main component which will eventually contain a slides container and card container(s).

---
+++ client/src/components/main/Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Grid, Container } from './main.styles';
+
+const Main = ({ children, ...restProps }) => {
+  return <Container {...restProps}>{children}</Container>;
+};
+
+Main.Grid = ({ children, ...restProps }) => {
+  return <Grid {...restProps}>{children}</Grid>;
+};
+
+export default Main;
